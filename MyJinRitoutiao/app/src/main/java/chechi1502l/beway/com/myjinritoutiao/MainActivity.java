96ab9d46a188e5fd9f16a915928ef11ec71d2c52
keai90008000 <415780449@qq.com>
2017-03-09 18:29:14 +0800

    一
    Signed-off-by: keai90008000 <415780449@qq.com>

---
+++ MyJinRitoutiao/app/src/main/java/chechi1502l/beway/com/myjinritoutiao/MainActivity.java
@@ -0,0 +1,13 @@
+package chechi1502l.beway.com.myjinritoutiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
